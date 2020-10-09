f5be2c86bfc2bb0d66e5bc4f16b550c0ec78716d
Karanko Andrei <andr.karanko@gmail.com>
2020-10-09 18:00:34 +0300

    Домашнее задание по первому уроку

---
+++ lessons/js/script.js
@@ -0,0 +1,22 @@
+// console.log(2);
+// console.log(3);
+
+// console.log('agePerson', agePerson);
+// const agePerson = 20;
+// console.log('agePerson', agePerson);
+
+// {
+// let agePerson = 20;
+// console.log('agePerson', agePerson);
+// }
+
+let money = "usd",
+  income = 1000,
+  addExpenses = 200,
+  deposit = 500,
+  mission = "find a job",
+  period = "half a year";
+
+alert("How's it hanging?");
+
+console.log("I want to", mission, "within", period);
