353fb5d3c8ac87d9c205043cd702ec8a9e7a4874
Karanko Andrei <andr.karanko@gmail.com>
2020-10-09 18:06:44 +0300

    Домашнее задание по уроку 2

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,22 +1,18 @@
-// console.log(2);
-// console.log(3);
+let money = 1000,
+  income = "Фриланс",
+  addExpenses = "продукты, транспорт, коммуналка, Интернет",
+  deposit = true,
+  mission = 6000,
+  period = 6,
+  budgetDay;
 
-// console.log('agePerson', agePerson);
-// const agePerson = 20;
-// console.log('agePerson', agePerson);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+console.log(addExpenses.toLowerCase().split(", "));
 
-// {
-// let agePerson = 20;
-// console.log('agePerson', agePerson);
-// }
-
-let money = "usd",
-  income = 1000,
-  addExpenses = 200,
-  deposit = 500,
-  mission = "find a job",
-  period = "half a year";
-
-alert("How's it hanging?");
-
-console.log("I want to", mission, "within", period);
+budgetDay = money / 30;
+console.log("budget per day", budgetDay);
