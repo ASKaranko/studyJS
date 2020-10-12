e3512e9b8085805b0a987ec241623d193b2575ec
Karanko Andrei <andr.karanko@gmail.com>
2020-10-12 22:14:23 +0300

    Домашнее задание по уроку номер 3 Динамическая типизация

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -16,3 +16,37 @@ console.log(addExpenses.toLowerCase().split(", "));
 
 budgetDay = money / 30;
 console.log("budget per day", budgetDay);
+
+// Ответы по уроку номер 3
+
+money = +prompt("Какой ваш месячный доход?", "1000");
+if (money !== money) {
+  confirm("Введите ответ в виде числа");
+  money = +prompt("Какой ваш месячный доход?", "1000");
+}
+
+addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "Кварплата, проездной, кредит");
+
+deposit = confirm("Есть ли у вас депозит в банке?");
+
+let expenses1 = prompt("Введите обязательную статью расходов номер 1?", "Кварплата");
+let amount1 = +prompt("Во сколько это обойдется?", "100");
+let expenses2 = prompt("Введите обязательную статью расходов номер 2?", "Интернет");
+let amount2 = +prompt("Во сколько это обойдется?", "50");
+
+let budgetMonth = money - (amount1 + amount2);
+console.log("Бюджет на месяц", budgetMonth);
+
+period = Math.ceil(mission / budgetMonth);
+console.log("Ваша цель будет достигнута через " + period + " месяцев");
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log("Ваш бюджет на день", budgetDay);
+
+if (budgetDay >= 1200) {
+  console.log("У вас высокий уровень дохода");
+} else if(budgetDay < 1200 && budgetDay >=600) {
+  console.log("У вас средний уровень дохода");
+} else if(budgetDay < 600 && budgetDay >=0) {
+  console.log("К сожалению у вас уровень дохода ниже среднего");
+}
